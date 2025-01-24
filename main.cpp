60d60461add10d579ae901467e102a59db19149e
Bapt59 <bapt5962@gmail.com>
2025-01-24 10:30:29 +0100

    Added messages

--- main.cpp
+++ main.cpp
@@ -28,10 +28,14 @@ int main(int argc, char* argv[])
     std::string file = argv[1];
     std::string outputFolder = argv[2];
 
+    std::cout << "Generating code..." << std::endl;
+
     Reader reader(file);
     Associator associator(reader.getNodes(), reader.getEdges());
     Coder coder(associator.getSteps(), associator.getActions(), associator.getTransitions());
     Writer writer(coder.getCodeSteps(), coder.getGlobalVariables(), coder.getPinmodes(), outputFolder);
+
+    std::cout << "Code generated." << std::endl;
     #endif
 
     return 0;
